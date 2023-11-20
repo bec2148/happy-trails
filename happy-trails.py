2d03441fdd58cb6fae91420e25b9697bcf4fdde7
Brendan Cunnie <saintbrendan@gmail.com>
2023-11-19 21:38:09 -0500

    WIP:  not working.  Sharing with Lukas.

--- happy-trails.py
+++ happy-trails.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, flash, redirect
+# ...
 # https://hevodata.com/learn/flask-mysql/
 from flask_mysqldb import MySQL
+from pattern.text.en import singularize
 import re
+
 
 regex_id = re.compile(r"Id$")
 
@@ -13,11 +16,12 @@ app.config['MYSQL_PASSWORD'] = 'my-secret-pw'
 app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
 
-def list(table):
+def list(table, can_insert):
     cursor = mysql.connection.cursor()
     query = (f"SELECT * from flask.{table};")
     cursor.execute(query)
-    print("cursor.description ", cursor.description)
+    # print("cursor.description ", cursor.description)
+    # cursor.description  (('id', 8, 2, 20, 20, 0, 0), ('first_name', 253, 11, 300, 300, 0, 1), ('last_name', 253, 13, 300, 300, 0, 1)
     headers = ""
     for field in cursor.description:
         column_title = field[0].title().replace("_"," ")
@@ -30,8 +34,38 @@ def list(table):
             rows += f"<td>{field}</td>"
         rows += "</tr>"
     cursor.close()
+    # https://stackoverflow.com/questions/31387905/converting-plural-to-singular-in-a-text-file-with-python
+    singular_table = singularize(table)
+    print("can_insert ", can_insert)
 
-    return render_template("table.html", table=table.title(), headers=headers, rows=rows)
+    return render_template("table.html", table=table.title(), headers=headers, rows=rows, can_insert=can_insert, singular_table=singular_table)
+
+def create(table, form):
+    print(f"form {form}")
+
+
+    title = form['first_name']
+    content = form['last_name']
+    print(f"title {title}  content {content}")
+
+    if not title:
+        flash('Title is required!')
+    elif not content:
+        flash('Content is required!')
+
+
+    return list(table, can_insert=True)
+
+
+    columns = [field.name for field in form]
+    values = [field.data for field in form]
+    print(f"columns {columns}  values {values}")
+    insert = "INSERT INTO flask.{{table}} ({{','.join(columns)}}) VALUES ({{','.join(values)}})"
+    cursor = mysql.connection.cursor()
+    ret = cursor.execute(insert)
+    print(f"ret {ret}")
+    cursor.close
+    ### redirect
 
 @app.route("/")
 def welcome():
@@ -52,32 +86,38 @@ def welcome():
     cursor.close()
     return render_template("table.html", table="Tables", headers=headers, rows=rows)
 
-@app.route("/students")
-def students():
-    cursor = mysql.connection.cursor()
-    query = ("SELECT * from flask.students;")
-    cursor.execute(query)
-    print("cursor.description ", cursor.description)
-    headers = ""
-    for field in cursor.description:
-        column_title = field[0].title().replace("_"," ")
-        column_title = regex_id.sub("ID", column_title)
-        headers += f"<th>{column_title}</th>"
-    rows = ""
-    for fields in cursor:
-        rows += "<tr>"
-        for field in fields:
-            rows += f"<td>{field}</td>"
-        rows += "</tr>"
-    cursor.close()
+# @app.route("/students")
+# def students():
+#     cursor = mysql.connection.cursor()
+#     query = ("SELECT * from flask.students;")
+#     cursor.execute(query)
+#     print("cursor.description ", cursor.description)
+#     headers = ""
+#     for field in cursor.description:
+#         column_title = field[0].title().replace("_"," ")
+#         column_title = regex_id.sub("ID", column_title)
+#         headers += f"<th>{column_title}</th>"
+#     rows = ""
+#     for fields in cursor:
+#         rows += "<tr>"
+#         for field in fields:
+#             rows += f"<td>{field}</td>"
+#         rows += "</tr>"
+#     cursor.close()
 
-    return render_template("table.html", table="Students", headers=headers, rows=rows)
+#     return render_template("table.html", table="Students", headers=headers, rows=rows)
 
 # https://stackoverflow.com/questions/14023864/flask-url-route-route-all-other-urls-to-some-function
 # under construction:  pull table name from url
 @app.route('/<first>', methods = ['POST', 'GET'])
 @app.route('/<first>/<path:rest>', methods = ['POST', 'GET'])
 def fallback(first=None, rest=None):
-    print(f'first {first} rest {rest}')
+    print(f"first {first}  rest {rest}  request.method=='POST' {request.method=='POST'}")
     if rest is None:
-        return list(first)
+        return list(first, can_insert=True)
+    if rest == "new":
+        return render_template("new.html", table=first)
+    if rest == "create" and request.method == 'POST':
+        return create(first, request.form)
+
+
